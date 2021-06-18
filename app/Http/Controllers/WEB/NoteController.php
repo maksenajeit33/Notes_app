e113990c9580709050f13e4f200490d283d0ea38
maksenajeit33 <dragongreen64@gmail.com>
2021-06-18 07:48:43 +0000

    Added CRUD functions for the notes [WEB]

---
+++ app/Http/Controllers/WEB/NoteController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\WEB;
+
+use App\Http\Controllers\Controller;
+use App\Models\Note;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $notes = Note::where('user_id', Auth::id())->latest()->paginate(5);
+
+        return view('notes.index', compact('notes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('notes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $value = $request->all();
+        $value['user_id'] = Auth::id();
+        $note = Note::create($value);
+
+        return redirect()->back()->with('success', 'Note created successful!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $note = Note::findOrFail($id);
+
+        return view('notes.show', compact('note'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $note = Note::findOrFail($id);
+
+        return view('notes.edit', compact('note'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $note = Note::findOrFail($id);
+
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $note->update($request->all());
+
+        return redirect()->back()->with('success', 'Note updated successful!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $note = Note::findOrFail($id);
+
+        $note->delete();
+
+        return redirect()->back()->with('success', 'Note deleted successful!');
+    }
+}
