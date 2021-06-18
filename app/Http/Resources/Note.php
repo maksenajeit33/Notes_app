1286b96ff56843aa61ed29582260a4f1a215169c
maksenajeit33 <dragongreen64@gmail.com>
2021-06-18 04:36:25 +0000

    Added Resources/Note.php file for SEND THE DATA THAT REQUESTED FROM THE DATABASE AS A JSON FORMAT

---
+++ app/Http/Resources/Note.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Note extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+
+        return [
+            'title' => $this->title,
+            'content' => $this->content,
+            'user' => $this->user->name,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y')
+        ];
+    }
+}
